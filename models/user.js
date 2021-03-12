2dddb90d4e703c3b96b9e57384bca547ecdc31b4
Aleksei Filatov <aleksei_filatov@epam.com>
2021-03-12 11:39:39 +0300

    model update

--- models/user.js
+++ models/user.js
@@ -1,17 +1,6 @@
-import Sequelize from "sequelize";
-
-const url =
-  "postgres://xfginkprwojuqb:f5d819380a3e46768b735fdeaf4f0ecfc2ab40176601187fca6d1cb946647a6e@ec2-176-34-222-188.eu-west-1.compute.amazonaws.com:5432/dcdnkedictj1ka";
-
-export const sequelize = new Sequelize(url, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: false,
-      rejectUnauthorized: false,
-    },
-  },
-});
+import pkg from "sequelize";
+const { Sequelize } = pkg;
+import { sequelize } from "../data-access/sequelize_dbconnect.js";
 
 export const User = sequelize.define("user", {
   id: {
